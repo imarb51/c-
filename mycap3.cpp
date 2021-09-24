6044f3c1c8b17508c4f54b9d521de1423463f0c4
imarb51 <80340473+imarb51@users.noreply.github.com>
2021-09-24 14:09:39 +0530

    Add files via upload

--- mycap3.cpp
+++ mycap3.cpp
@@ -7,20 +7,49 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <iostream>
-
+#include <iomanip>
+ 
 using namespace std;
-
+ 
+class Time
+{
+    private:
+        int seconds;
+        int hh,mm,ss;
+    public:
+        void getTime(void);
+        void convertIntoSeconds(void);
+        void displayTime(void);
+};
+ 
+void Time::getTime(void)
+{
+    cout << "Enter time:" << endl;
+    cout << "Hours?   ";          cin >> hh;
+    cout << "Minutes? ";          cin >> mm;
+    cout << "Seconds? ";          cin >> ss;
+}
+ 
+void Time::convertIntoSeconds(void)
+{
+    seconds = hh*3600 + mm*60 + ss;
+}
+ 
+void Time::displayTime(void)
+{
+    cout << "The time is = " << setw(2) << setfill('0') << hh << ":"
+                             << setw(2) << setfill('0') << mm << ":"
+                             << setw(2) << setfill('0') << ss << endl;
+    cout << "Time in total seconds: " << seconds;
+}
+ 
 int main()
 {
-    int age;
-    std::cout << "enter your age" << std::endl;
-    std::cin >> age;
-    if(age>=18){
-        cout<<"\nYou are eligible for voting";
-    }
-    else{
-        cout<<"\n You are not eligible for voting"
-    }
-
+    Time T; //creating objects
+     
+    T.getTime();
+    T.convertIntoSeconds();
+    T.displayTime();
+     
     return 0;
 }
